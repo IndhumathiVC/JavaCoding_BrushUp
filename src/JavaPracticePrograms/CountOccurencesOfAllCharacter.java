7b441c2c370f4774a8bfd8c71c1f511f397dc537
Indhumathi V C <505068797@penske.com>
2026-02-11 19:32:39 +0530

    Strings & Arrays Practice

--- src/JavaPracticePrograms/CountOccurencesOfAllCharacter.java
+++ src/JavaPracticePrograms/CountOccurencesOfAllCharacter.java
@@ -2,13 +2,14 @@ package JavaPracticePrograms;
 
 public class CountOccurencesOfAllCharacter {
     public static void main(String [] args){
-        String str = "Madam";
+        String str = "Code Execution Successful";
         str = str.toLowerCase();
         
         for(int i=0; i<str.length(); i++) {
             char ch = str.charAt(i);
             if (str.indexOf(ch) == i) {
                 int count = 0;
+                if(ch == ' ') continue;
                 for (int j = 0; j < str.length(); j++) {
                     if (str.charAt(j) == ch) {
                         count++;
